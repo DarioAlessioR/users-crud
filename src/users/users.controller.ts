437de21b00481f91f8c3e18ed900c5b0a393eb70
DarioAlessioR <100212928+DarioAlessioR@users.noreply.github.com>
2024-01-03 18:52:09 +0100

    Update users.controller.ts
    create findById method

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, HttpStatus, Param } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  @Get()
+  async findAll() {
+    return [{ id: 1, name: "dario" }, { id: 2, name: "dario2" }, { id: 3, name: "dario3"}];
+  }
+
+  @Get(':id')
+  @HttpCode(HttpStatus.OK)
+  async findById(@Param('id') id: number) {
+    return { id: id, name: "dario" };
+  }
+
+}
